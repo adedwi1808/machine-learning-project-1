e7bee38a3660dc1347c8241cc24ba5fafe24b5b7
Ade Dwi Prayitno <adedwip1808@gmail.com>
2025-03-17 20:03:26 +0700

    feat: model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -47,4 +48,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path=train_data, test_path=test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path=train_data, test_path=test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
+    
